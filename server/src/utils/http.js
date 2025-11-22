6722f59563c949fdaaee8ff2b5444dff259a12d6
Alejandra Zepeda <msc_azepeda2024@accitesz.com>
2025-11-22 12:04:25 -0600

    inicial

--- server/src/utils/http.js
+++
@@ -1,7 +0,0 @@
-function ok(res, data, status = 200) {
-  return res.status(status).json(data)
-}
-function fail(res, error, status = 500) {
-  return res.status(status).json({ error })
-}
-module.exports = { ok, fail }
