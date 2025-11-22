271892fd84c6d9be0ab619f6f4ed795ee6950551
Alejandra Zepeda <msc_azepeda2024@accitesz.com>
2025-11-22 11:51:19 -0600

    separar backend

--- client/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from '../auth.jsx'
-import { Navigate } from 'react-router-dom'
-
-export default function ProtectedRoute({ roles = [], children }) {
-  const { user } = useAuth()
-  if (!user) return <Navigate to="/login" />
-  if (roles.length && !roles.includes(user.rol)) return <Navigate to="/" />
-  return children
-}
